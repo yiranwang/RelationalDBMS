e884540719c9e0de998dbaac5a8e311c9d1f131d
oldsui <rayliu317612@gmail.com>
2016-09-27 14:44:21 -0700

    added clean and runTest script

---
+++ codebase/rbf/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+make clean
+rm test3
+rm test7
---
+++ codebase/rbf/runTest.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+./rbftest1
+./rbftest2
+./rbftest3
+./rbftest4
+./rbftest5
+./rbftest6
+./rbftest7
+#./rbftest8
+#./rbftest9
+#./rbftest10
+#./rbftest11
+#./rbftest12
