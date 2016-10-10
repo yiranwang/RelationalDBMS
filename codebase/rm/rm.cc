9ef039a872afa90803d446bf9c7838c44a99da18
violayueer <irisyueer@gmail.com>
2016-10-10 00:20:52 -0700

    rm_createCatalog

--- codebase/rm/rm.cc
+++ codebase/rm/rm.cc
@@ -1,5 +1,16 @@
 
 #include "rm.h"
+#include <stdlib.h>
+
+
+void createTableRecordDescriptor(vector<Attribute> &recordDescriptor);
+
+void createColumnRecordDescriptor(vector<Attribute> &recordDescriptor);
+
+void prepareTableRecord(vector<Attribute> &recordDescriptor);
+
+void prepareColumnRecord(vector<Attribute> &recordDescriptor);
+
 
 RelationManager* RelationManager::_rm = 0;
 
@@ -21,6 +32,30 @@ RelationManager::~RelationManager()
 
 RC RelationManager::createCatalog()
 {
+	//create table record descriptor -> prepare table record -> insert
+	//create column record descriptor -> prepare column record -> insert
+	FileHandle tableFileHandle;
+	RID tableRid;
+
+	void *tableData = malloc(PAGE_SIZE);
+	unsigned char *nullIndicator = (unsigned char *)malloc(PAGE_SIZE);
+
+	memset(nullIndicator, 0, sizeof(char));
+
+	vector<Attribute> tableRecordDescriptor;
+
+	//create "table-id", "table-name", "file-name" record desciptor in catalog table
+	createTableRecordDescriptor(tableRecordDescriptor);
+
+	rbfm->createFile("Tables");
+	rbfm->openFile("Tables", tableFileHandle);
+
+	//insert records of table "Tables", "Columns" to "Table"
+	int tableRecordSize = 0;
+	prepareTableRecord(nullIndicator, 1, "Tables", "Tables", tableData, tableRecordSize);
+	rbfm->insertRecord(FileHandle, tableRecordDescriptor, tableData, tableRid);
+
+
     return -1;
 }
 
@@ -96,4 +131,80 @@ RC RelationManager::addAttribute(const string &tableName, const Attribute &attr)
     return -1;
 }
 
+void createTableRecordDescriptor(vector<Attribute> &recordDescriptor) {
+	Attribute attr1;
+	attr1.name = "table-id";
+	attr1.type = TypeInt;
+	attr1.length = (AttrLength)4;
+	recordDescriptor.push_back(attr1);
 
+	Attribute attr2;
+	attr2.name = "table-name";
+	attr2.type = TypeVarChar;
+	attr2.length = (AttrLength)50;
+	recordDescriptor.push_back(attr2);
+
+	Attribute attr3;
+	attr3.name = "file-name";
+	attr3.type = TypeVarChar;
+	attr3.length = (AttrLength)50;
+	recordDescriptor.push_back(attr3);
+}
+
+void createColumnRecordDescriptor(vector<Attribute> &recordDescriptor) {
+	Attribute attr1;
+	attr1.name = "table-id";
+	attr1.type = TypeInt;
+	attr1.length = (AttrLength)4;
+	recordDescriptor.push_back(attr1);
+
+	Attribute attr2;
+	attr2.name = "column-name";
+	attr2.type = TypeVarChar;
+	attr2.length = (AttrLength)50;
+	recordDescriptor.push_back(attr2);
+
+	Attribute attr3;
+	attr3.name = "column-type";
+	attr3.type = TypeInt;
+	attr3.length = (AttrLength)4;
+	recordDescriptor.push_back(attr3);
+
+	Attribute attr4;
+	attr4.name = "column-length";
+	attr4.type = TypeInt;
+	attr4.length = (AttrLength)4;
+	recordDescriptor.push_back(attr4);
+
+	Attribute attr5;
+	attr5.name = "column-position";
+	attr5.type = TypeInt;
+	attr5.length = (AttrLength)4;
+	recordDescriptor.push_back(attr5);
+}
+
+void prepareTableRecord(unsigned char *nullIndicator, const int tableId, const string &tableName, const string &fileName, void *data, int &recordSize) {
+	int tableNameLength = tableName.size();
+
+	int fileNameLength = fileName.size();
+
+	int offset = 0;
+
+	memcpy(data, nullIndicator, sizeof(char));
+
+	offset += sizeof(char);
+
+	*((int*)((char*)data + offset)) = tableId;
+
+	offset += sizeof(int);
+
+	memcpy((char*)data + offset, tableName.c_str(), tableNameLength);
+
+	offset += tableNameLength;
+
+	memcpy((char*)data + offset, fileName.c_str(), fileNameLength);
+
+	offset += fileNameLength;
+
+	recordSize = offset;
+}
